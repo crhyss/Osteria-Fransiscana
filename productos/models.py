1ba3933a90b69bbee303a993581e8e5501384e3d
El-Lexhu <yer.dro.ort@gmail.com>
2022-09-28 20:21:03 -0300

    avance bd

---
+++ productos/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Categoria_prod(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    categoria_prod = models.CharField(max_length=40, blank=False)
+
+class Producto:
+    id_producto = models.AutoField(primary_key=True)
+    prod_nombre = models.CharField(max_length=100, blank=False)
+    prod_descri = models.CharField(max_length=200, blank=False)
+    prod_precio_ba = models.IntegerField(blank=False)
+    prod_precio_of = models.IntegerField(blank=False)
+    prod_imagen    = models.FileField(upload_to='/productos/imagenes', default=None)
+    prod_categoria = models.ForeignKey(Categoria_prod, on_delete=models.CASCADE, default=None)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Tipo_usuario(models.Model):
+    id_tipo_usr = models.AutoField(primary_key=True)
+    tipo_usr = models.CharField(max_length=40)
+
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    usr_nombre = models.CharField(max_length=40, blank=False)
+    usr_apellido_pat = models.CharField(max_length=40, blank=False)
+    usr_apellido_mat = models.CharField(max_length=40, blank=False)
+    usr_pass = models.CharField(max_length=40, blank=False)
+    usr_correo = models.CharField(max_length=100, blank=False)
+    usr_activo = models.BooleanField(default=True)
+    usr_tipo = models.ForeignKey(Tipo_usuario, on_delete=models.CASCADE, default=None)
